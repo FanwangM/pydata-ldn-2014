6f87f5d5f72ad1dbe793747cac37a5678e4d48c0
Simon Jagoe <simon@simonjagoe.com>
2014-02-20 23:57:12 +0000

    Add console action to get IPython console

--- stackexchange/stackimport/main.py
+++ stackexchange/stackimport/main.py
@@ -15,12 +15,29 @@ def connect(db_name):
 
 
 def parse_args():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog='stackimport')
     parser.add_argument('db_name')
-    parser.add_argument('forum_name')
-    parser.add_argument('file')
     parser.add_argument('--create', action='store_true', default=False)
+
+    subparsers = parser.add_subparsers()
+    import_group = subparsers.add_parser('import')
+    import_group.add_argument('forum_name')
+    import_group.add_argument('file')
+    import_group.set_defaults(func=import_action)
+
+    console_group = subparsers.add_parser('console')
+    console_group.set_defaults(func=console_action)
     return parser.parse_args(sys.argv[1:])
+
+
+def import_action(session, args):
+    import_forum(session, args.forum_name, args.file)
+
+
+def console_action(session, args):
+    from . import dbmodels  # noqa
+    import IPython
+    IPython.embed()
 
 
 def main():
@@ -29,4 +46,4 @@ def main():
     if args.create:
         Base.metadata.create_all(engine)
 
-    import_forum(session, args.forum_name, args.file)
+    args.func(session, args)
